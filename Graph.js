77708e0b75cf0cb3c26253f47f7c5398696a44b1
Yassine Zaoui - 18 yo <y.zaoui.pro@gmail.com>
2022-07-28 06:52:49 +0200

    ajouter des event a une classe c'est vraiment galère

--- Graph.js
+++ Graph.js
@@ -17,7 +17,7 @@ const pointB = document.getElementById("B")
 
 const pointC = document.getElementById("C")
 
-let Nodes = document.querySelectorAll(".Noeud")
+let Nodes = []
 
 let id = 0
 
@@ -35,7 +35,7 @@ CreateNode.addEventListener("click",Creation_Noeud)//quand on apuye sur le bouto
 function Creation_Noeud() {
 
     debut = true;
-    console.log("test");//j'affiche test de la console
+    // console.log("test");//j'affiche test de la console
     var name = document.createElement("p");
     name.setAttribute("class","blabla");
     var txt = document.createTextNode(id);
@@ -60,7 +60,7 @@ NewGraph.addEventListener("click",Stoper_propagation);//creation de l'event Stop
 
 
 function Stoper_propagation(){//Je veux que quand je click la div ne bouge plus 
-    console.log("je click sur le cercle");//j'affiche
+    // console.log("je click sur le cercle");//j'affiche
     if (debut){
         //("1. creation du noeud")
         var FixGraph = document.createElement("div");
@@ -82,119 +82,148 @@ function Stoper_propagation(){//Je veux que quand je click la div ne bouge plus
         
         NewGraph.style.visibility = "hidden";//je rend visible la div mousemove
         NewGraph.style.left ="0 px";
-        NewGraph.style.top = "0 px";//La div suit la souris   
-        Creation_liason();
+        NewGraph.style.top = "0 px";//La div suit la souris 
+        
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        // console.log(document.getElementById("Noeud"+(id-1)))
+        Nodes = document.querySelectorAll(".Noeud")
+        tour = 0
+        // console.log("Node === ",Nodes,"|len =",Nodes.length)
+        // crée une liason
+        // console.log(Nodes)
+        Nodes.forEach((Noeud)=>{
+            Noeud.addEventListener("click",()=>{
+                let mtn = Noeud.id;
+                console.log("tour =",tour)
+                if (tour==0){//vrai uniquement le premier tour
+                    /////etape 1 trouver l'index de ce noeud dans Nodes
+                    console.log("mtn = ",mtn);
+                    index_for_liason = couper_STR(mtn,5,mtn.length);
+                    console.log("index for liason = ",index_for_liason);
+                    //le problème n'est pas encore regler
+                
+                }
+                tour++;//etape 2 vu que d'es le premier toure la condition s'anulle il ne re executera plus la fonction
+
+                if (index_for_liason+tour>=Nodes.length){//si le nombre de tour + Index = a la longuer on reset
+                    tour=0;
+                }
+
+            })
+        });    
     }
     debut = false;
 }
-
 // a chaque fois que je finis de crée un nouveaux graphe je peux crée des liason entre chaque graphe 
 //
 let tour = 0;
-let Number_of_Node = 0;
 let index_for_liason = 0;
-function Creation_liason () {
-    Nodes = document.querySelectorAll(".Noeud")
-    Number_of_Node = Nodes.length;
-//trop galere de faire les event avec le la classe for each
+// function Creation_liason () {
+    ///////////////////////////////////////////////////////////////////////////////////////////////:
     // // console.log("Node === ",Nodes)
     // // crée une liason
     // console.log(Nodes)
     // Nodes.forEach((Noeud)=> {
-    //     if (tour==0){
-    //         console.log("-----------DEBU--------------")
-    //     }
-    //     Noeud.addEventListener("click",()=>{//(quand il est une fois vrai il et vrais pour la suite de la liste cela pose problème :( )
-    //         if (tour==index_for_liason) {
-    //             index_for_liason--
+        //     if (tour==0){
+            //         console.log("-----------DEBU--------------")
+            //     }
+            //     Noeud.addEventListener("click",()=>{//(quand il est une fois vrai il et vrais pour la suite de la liste cela pose problème :( )
+            //         if (tour==index_for_liason) {
+                //             index_for_liason--
     //             console.log("ICI = ",Noeud.id)
     //             console.log("2. creation d'une liason")
     //         // console.log(connexion)
     //         if (connexion== true){
-    //             Nodes.forEach((N)=>{
-    //                 N.style.background = "black"
-    //             })
-    //             console.log("cas 1 ")
-    //             connexion = false;
-    //         }else{
-    //             console.log("cas 2")
-
-    //             // je separe en deux 
-    //             NewBarre.style.top = Noeud.style.top;
-    //             // console.log(NewBarre.style.top)
+        //             Nodes.forEach((N)=>{
+            //                 N.style.background = "black"
+            //             })
+            //             console.log("cas 1 ")
+            //             connexion = false;
+            //         }else{
+                //             console.log("cas 2")
                 
-    //             NewBarre.style.left = Noeud.style.left;
-    //             // console.log(NewBarre.style.left)
+                //             // je separe en deux 
+                //             NewBarre.style.top = Noeud.style.top;
+                //             // console.log(NewBarre.style.top)
                 
-    //             NewBarre.style.visibility = "visible"
-    //             // console.log(NewBarre.style.visibility)
-    //             pointA.style.top = Noeud.style.top;
-    //             pointA.style.left = Noeud.style.left
-    //             pointC.style.top = Noeud.style.top
+                //             NewBarre.style.left = Noeud.style.left;
+                //             // console.log(NewBarre.style.left)
                 
-    //             html.addEventListener("mousemove",Mouvement_souris_barre)
+                //             NewBarre.style.visibility = "visible"
+                //             // console.log(NewBarre.style.visibility)
+                //             pointA.style.top = Noeud.style.top;
+                //             pointA.style.left = Noeud.style.left
+                //             pointC.style.top = Noeud.style.top
                 
-    //             // console.log("test liason");
-    //             // console.log("id = Noeud"+Noeud.textContent);
-    //             // console.log("ici = ",Nodes)// pourquoi il ne prend pas tout les Nodes ?? // il doit y avoir un truc avec les paire impaire mais jsp pk ?
-    //             Nodes.forEach((N)=>{
-    //                 N.style.background = "blue"
-    //             })
-    //             connexion = true
-    //             }
-    //         }
-    //     });
-    //     console.log("nbr de tour = ",tour)
+                //             html.addEventListener("mousemove",Mouvement_souris_barre)
+                
+                //             // console.log("test liason");
+                //             // console.log("id = Noeud"+Noeud.textContent);
+                //             // console.log("ici = ",Nodes)// pourquoi il ne prend pas tout les Nodes ?? // il doit y avoir un truc avec les paire impaire mais jsp pk ?
+                //             Nodes.forEach((N)=>{
+                    //                 N.style.background = "blue"
+                    //             })
+                    //             connexion = true
+                    //             }
+                    //         }
+                    //     });
+                    //     console.log("nbr de tour = ",tour)
     //     tour++;
     //     index_for_liason++;
-    //     if (tour>=Number_of_Node){
-    //         console.log("tour =",tour," | Number of node = ",Number_of_Node," | index for liason = ",index_for_liason)
-    //         tour=0;
-    //         index_for_liason = 0 ;
-    //         console.log("----------FIN---------------")
-    //     }
-    // });
-}
-
-
-
-//crée le point A B C pour savoir ou est chaque point important
-
-
-
-/// fonction qui permet de bien manipuler la barre
-function Mouvement_souris_barre(e) {
-    // console.log("test de la fonction mouvement souris")
-    //aplication fo DOM
-    NewBarre.style.background = "red";
-    // deplacement du point B constant
-    pointB.style.left =e.pageX+"px";
-    pointB.style.top =e.pageY+"px";
-    pointC.style.left = e.pageX+"px"
-    //calule des segement
-     
-    let AC = (e.pageX-parseInt(pointA.style.left));
-    let BC = (e.pageY-parseInt(pointC.style.top));
-    
-    // console.log(pointA.style.left)
-    let hyphotenus = Math.pow(AC,2)+Math.pow(BC,2);
-    
-    // console.log(hyphotenus)
-    hyphotenus = Math.sqrt(hyphotenus);
-    //sqrt( (Xb - Xa)**2 + (Yb - Ya)**2 ) = la distance du segement AB
-//////////////////////////////////// Barre et les angles
-    NewBarre.style.left = parseInt(pointA.style.left)- hyphotenus/2+'px';
-    NewBarre.style.width = hyphotenus-5+"px";//petit decallage sinon compliquer pour apuyer les noeud
-    let tan = BC/AC
-
-    let angleCAB = Math.atan(tan)*180/3.14;
-
-    if (AC<0) {
-        NewBarre.style.transform = "rotate("+angleCAB+"deg) translate(-50%,-50%)";
-    } else {
-        NewBarre.style.transform = "rotate("+angleCAB+"deg) translate(50%,50%)";
+    //     if (tour>=Nodes.length){
+        //         console.log("tour =",tour," | Number of node = ",Nodes.length," | index for liason = ",index_for_liason)
+        //         tour=0;
+        //         index_for_liason = 0 ;
+        //         console.log("----------FIN---------------")
+        //     }
+        // });
+        //}
+        
+        
+        
+        //crée le point A B C pour savoir ou est chaque point important
+        
+        /// fonction qui permet de bien manipuler la barre
+        function Mouvement_souris_barre(e) {
+            // console.log("test de la fonction mouvement souris")
+            //aplication fo DOM
+            NewBarre.style.background = "red";
+            // deplacement du point B constant
+            pointB.style.left =e.pageX+"px";
+            pointB.style.top =e.pageY+"px";
+            pointC.style.left = e.pageX+"px"
+            //calule des segement
+            
+            let AC = (e.pageX-parseInt(pointA.style.left));
+            let BC = (e.pageY-parseInt(pointC.style.top));
+            
+            // console.log(pointA.style.left)
+            let hyphotenus = Math.pow(AC,2)+Math.pow(BC,2);
+            
+            // console.log(hyphotenus)
+            hyphotenus = Math.sqrt(hyphotenus);
+            //sqrt( (Xb - Xa)**2 + (Yb - Ya)**2 ) = la distance du segement AB
+            //////////////////////////////////// Barre et les angles
+            NewBarre.style.left = parseInt(pointA.style.left)- hyphotenus/2+'px';
+            NewBarre.style.width = hyphotenus-5+"px";//petit decallage sinon compliquer pour apuyer les noeud
+            let tan = BC/AC
+            
+            let angleCAB = Math.atan(tan)*180/3.14;
+            
+            if (AC<0) {
+                NewBarre.style.transform = "rotate("+angleCAB+"deg) translate(-50%,-50%)";
+            } else {
+                NewBarre.style.transform = "rotate("+angleCAB+"deg) translate(50%,50%)";
+            }
+            
+        }
+        
+        //quand on re apuye sur un noeud il se connecte crée un bouton annuler crée un le bouton suprimer noeur est crée une matrice qui contien l'id de chaque noeud avec keur liason
+        
+function couper_STR(S,D,F){
+    let str = '';
+    for (let i = 0; i < F-D; i++) {
+      str = str + S[i+D];
     }
-
+    return str
 }
-
-//quand on re apuye sur un noeud il se connecte crée un bouton annuler crée un le bouton suprimer noeur est crée une matrice qui contien l'id de chaque noeud avec keur liason
